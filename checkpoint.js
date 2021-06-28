e8c9b3bd963fbbfb196e2584d5ed2cb584560825
marcelo miyoshi <miyoshi_marcelo@hotmail.com>
2021-06-28 15:48:44 -0300

    usando switch

---
+++ checkpoint.js
@@ -0,0 +1,123 @@
+let microondas = (prato, tempo) => {
+    switch (prato) {
+        case 1:
+            if ((tempo >= 10) & (tempo < 20)) {
+                return 'Pipoca pronta, bom apetite!!!'
+            }
+            if ((tempo >= 20) & (tempo < 30)) {
+                return 'A Pipoca queimou'
+            }
+            if (tempo < 10) {
+                return 'Tempo insuficiente para o preparo da Pipoca'
+            }
+            if (tempo >= 30) {
+                return 'kabumm'
+            }
+            else
+                return 'Tempo indeterminado , usando tempo padrão , Pipoca pronta, bom apetite!!!'
+
+            break;
+        case 2:
+            if ((tempo >= 8) & (tempo < 16)) {
+                return 'Macarrão pronto, bom apetite!!!'
+            }
+            if ((tempo >= 16) & (tempo < 24)) {
+                return 'O Macarrão queimou'
+            }
+            if (tempo < 8) {
+                return 'Tempo insuficiente para o preparo do Macarrão'
+            }
+            if (tempo >= 24) {
+                return 'kabumm'
+            }
+            else
+                return 'Tempo indeterminado , usando tempo padrão , Macarrão pronto, bom apetite!!!'
+
+            break;
+        case 3:
+            if ((tempo >= 15) & (tempo < 30)) {
+                return 'Carne pronta, bom apetite!!!'
+            }
+            if ((tempo >= 30) & (tempo < 45)) {
+                return 'A Carne queimou'
+            }
+            if (tempo < 15) {
+                return 'Tempo insuficiente para o preparo da Carne'
+            }
+            if (tempo >= 45) {
+                return 'kabumm'
+            }
+            else
+                return 'Tempo indeterminado , usando tempo padrão , Carne pronta, bom apetite!!!'
+
+            break;
+        case 4:
+            if ((tempo >= 12) & (tempo < 24)) {
+                return 'Feijão pronto, bom apetite!!!'
+            }
+            if ((tempo >= 24) & (tempo < 36)) {
+                return 'O Feijão queimou'
+            }
+            if (tempo < 12) {
+                return 'Tempo insuficiente para o preparo do Feijão'
+            }
+            if (tempo >= 36) {
+                return 'kabumm'
+            }
+            else
+                return 'Tempo indeterminado , usando tempo padrão , Feijão pronto, bom apetite!!!'
+
+            break;
+        case 5:
+            if ((tempo >= 8) & (tempo < 16)) {
+                return 'Brigadeiro pronto, bom apetite!!!'
+            }
+            if ((tempo >= 16) & (tempo < 24)) {
+                return 'O Brigadeiro queimou'
+            }
+            if (tempo < 16) {
+                return 'Tempo insuficiente para o preparo do Brigadeiro'
+            }
+            if (tempo >= 24) {
+                return 'kabumm'
+            }
+            else
+                return 'Tempo indeterminado , usando tempo padrão , Brigadeiro pronto, bom apetite!!!'
+
+            break;
+        default: return "Prato inexistente"
+            break;
+    }
+
+}
+
+//menu//
+console.log('opção - 1 - Pipoca – 10 segundos (padrão)' + '\n' +
+    'opção - 2 - Macarrão – 8 segundos (padrão)' + '\n' +
+    'opção - 3 - Carne – 15 segundos (padrão)' + '\n' +
+    'opção - 4 - Feijão – 12 segundos (padrão)' + '\n' +
+    'opção - 5 - Brigadeiro – 8 segundos (padrão)')
+console.log(' ')
+
+console.log(microondas(3, 7)) //teste opção 3 , com tempo de 7 segundos ; resultado esperado : Tempo insuficiente para o preparo da Carne //
+console.log(microondas(3, 50)) //teste opção 3 , com tempo de 40 segundos ; resultado esperado : kabumm//
+console.log(' ')
+
+console.log(microondas(4, 10)) //teste opção 4 , com tempo de 10 segundos ; resultado esperado : Tempo insuficiente para o preparo do Feijão //
+console.log(microondas(4)) //teste opção 4 , sem tempo ; resultado esperado : Tempo indeterminado , usando tempo padrão , Feijão pronto, bom apetite!!!//
+console.log(' ')
+
+console.log(microondas(1, 10)) //teste opção 1 , com tempo de 10 segundos ; resultado esperado :Pipoca pronta, bom apetite!!!//
+console.log(microondas(1, 20)) //teste opção 1 , com tempo de 20 segundos ; resultado esperado : A Pipoca queimou//
+console.log(' ')
+
+console.log(microondas(2, 4)) //teste opção 2 , com tempo de 8 segundos ; resultado esperado : Tempo insuficiente para o preparo do Macarrão//
+console.log(microondas(2, 'p')) //teste opção 2 , com tempo = 'p' ; resultado esperado : Tempo indeterminado , usando tempo padrão , Macarrão pronto, bom apetite!!!//
+console.log(' ')
+
+console.log(microondas(5, 14)) //teste opção 5 , com tempo de 14 segundos ; resultado esperado : Brigadeiro pronto, bom apetite!!! //
+console.log(microondas(5, 24)) //teste opção 5 , com tempo de 7 segundos ; resultado esperado : kabumm//
+console.log(' ')
+
+console.log(microondas('sorvete', 7)) //teste opção sorvete , com tempo de 7 segundos ; resultado esperado : Prato inexistente//
+console.log(microondas(6, 50)) //teste opção 6 , com tempo de 50 segundos ; resultado esperado : Prato inexistente//
